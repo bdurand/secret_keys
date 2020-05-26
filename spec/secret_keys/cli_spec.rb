0682f9445820af2c823fdf0309caa4f126629af9
Brian Durand <bbdurand@gmail.com>
2020-05-25 18:28:38 -0700

    require cli in cli specs

--- spec/secret_keys/cli_spec.rb
+++ spec/secret_keys/cli_spec.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "../spec_helper"
+require_relative "../../lib/secret_keys/cli"
 
 describe SecretKeys::CLI do
   describe SecretKeys::CLI::Base do
@@ -9,11 +10,11 @@ describe SecretKeys::CLI do
       it "should read the secret key from the --secret-key=- option"
       it "should read the secret key from the --secret-key-file option"
     end
-    
+
     describe "output format option" do
       it "should set the output format from the --format option"
     end
-    
+
     describe "input and output files" do
       it "should default to using STDIN and STDOUT"
       it "should use STDIN and STDOUT if set to -"
@@ -21,12 +22,12 @@ describe SecretKeys::CLI do
       it "should be able to set the output to a file"
     end
   end
-  
+
   describe SecretKeys::CLI::Encrypt do
     it "should encrypt the input file"
-    
+
     it "should encrypt the input file using a new encryption key"
-    
+
     it "should encrypt the input file in place if the --in-place option is set"
   end
 
@@ -36,27 +37,27 @@ describe SecretKeys::CLI do
 
   describe SecretKeys::CLI::Read do
     it "should read a key from the input file"
-    
+
     it "should read a nested key from the input file"
-    
+
     it "should not output anything if the key does not exist"
   end
 
   describe SecretKeys::CLI::Edit do
     it "should add a key to the encrypted keys"
-    
+
     it "should move a key to the encrypted keys if it exists and no value was specified"
 
     it "should add a nested key to the encrypted keys"
 
     it "should add a key to the plain text keys"
-    
+
     it "should move a key to the plain text keys if it exists and no value was specified"
 
     it "should add a nested key to the plain text keys"
-    
+
     it "should remove a key from the encrypted keys"
-    
+
     it "should remove a key from the plain text keys"
   end
 end
