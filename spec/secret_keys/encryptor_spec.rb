20a633fc44342b18165aa82de779466382fbc579
Winston Durand <me@winstondurand.com>
2020-05-30 16:14:54 -0700

    Add a couple more specs

--- spec/secret_keys/encryptor_spec.rb
+++ spec/secret_keys/encryptor_spec.rb
@@ -3,34 +3,52 @@
 require_relative "../spec_helper"
 
 describe SecretKeys::Encryptor do
-  it "should encrypt a value with a password an salt" do
-    encryptor = SecretKeys::Encryptor.from_password("SECRET_KEY", "deadbeef")
-    encrypted = encryptor.encrypt("stuff")
-    expect(encryptor.decrypt(encrypted)).to eq "stuff"
+  let(:password) { "SECRET_KEY" }
+  let(:salt) { "deadbeef" }
+  let(:encryptor) { SecretKeys::Encryptor.from_password(password, salt) }
+
+  describe "#encrypt" do
+    it "should encrypt a value with a password an salt" do
+      encrypted = encryptor.encrypt("stuff")
+      expect(encryptor.decrypt(encrypted)).to eq "stuff"
+    end
+
+    it "should never encrypt a value the same way twice" do
+      encrypted_1 = encryptor.encrypt("stuff")
+      encrypted_2 = encryptor.encrypt("stuff")
+      expect(encrypted_1).to_not eq encrypted_2
+      expect(encryptor.decrypt(encrypted_2)).to eq "stuff"
+    end
+
+    it "should not encrypt a non-string" do
+      expect(encryptor.encrypt(1)).to eq 1
+      expect(encryptor.encrypt(false)).to eq false
+      expect(encryptor.encrypt(nil)).to eq nil
+    end
+
+    it "should not encrypt an empty string" do
+      encryptor = SecretKeys::Encryptor.from_password("SECRET_KEY", "deadbeef")
+      expect(encryptor.encrypt("")).to eq ""
+    end
   end
 
-  it "should never encrypt a value the same way twice" do
-    encryptor = SecretKeys::Encryptor.from_password("SECRET_KEY", "deadbeef")
-    encrypted_1 = encryptor.encrypt("stuff")
-    encrypted_2 = encryptor.encrypt("stuff")
-    expect(encrypted_1).to_not eq encrypted_2
-    expect(encryptor.decrypt(encrypted_2)).to eq "stuff"
+  describe ".from_password" do
+    it "should raise an error if password is empty" do
+      expect { SecretKeys::Encryptor.from_password(nil, salt) }.to raise_error(ArgumentError)
+      expect { SecretKeys::Encryptor.from_password("", salt) }.to raise_error(ArgumentError)
+    end
+
+    it "should raise an error if salt is invalid" do
+      expect { SecretKeys::Encryptor.from_password(password, nil) }.to raise_error(ArgumentError)
+      expect { SecretKeys::Encryptor.from_password(password, "") }.to raise_error(ArgumentError)
+      expect { SecretKeys::Encryptor.from_password(password, "d") }.to raise_error(ArgumentError)
+      expect { SecretKeys::Encryptor.from_password(password, "nothex") }.to raise_error(ArgumentError)
+    end
   end
 
-  it "should not encrypt a non-string" do
-    encryptor = SecretKeys::Encryptor.from_password("SECRET_KEY", "deadbeef")
-    expect(encryptor.encrypt(1)).to eq 1
-    expect(encryptor.encrypt(false)).to eq false
-    expect(encryptor.encrypt(nil)).to eq nil
-  end
-
-  it "should not encrypt an empty string" do
-    encryptor = SecretKeys::Encryptor.from_password("SECRET_KEY", "deadbeef")
-    expect(encryptor.encrypt("")).to eq ""
-  end
-
-  it "should not encrypt when the encryption key is nil or empty" do
-    expect { SecretKeys::Encryptor.from_password(nil, "deadbeef") }.to raise_error(ArgumentError)
-    expect { SecretKeys::Encryptor.from_password("", "deadbeef") }.to raise_error(ArgumentError)
+  describe ".new" do
+    it "should raise an error if key is incorrect length" do
+      expect { SecretKeys::Encryptor.new("too_short") }.to raise_error(ArgumentError)
+    end
   end
 end
